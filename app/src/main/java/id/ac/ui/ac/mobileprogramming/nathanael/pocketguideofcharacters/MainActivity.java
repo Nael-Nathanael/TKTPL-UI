392c8961b70e386a5e5fea63dacef883a8d4966c
Nathanael <nathanael71@ui.ac.id>
2020-09-28 22:44:54 +0800

    feat: complete basic main activity

--- app/src/main/java/id/ac/ui/ac/mobileprogramming/nathanael/pocketguideofcharacters/MainActivity.java
+++ app/src/main/java/id/ac/ui/ac/mobileprogramming/nathanael/pocketguideofcharacters/MainActivity.java
@@ -1,14 +1,34 @@
 package id.ac.ui.ac.mobileprogramming.nathanael.pocketguideofcharacters;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import id.ac.ui.ac.mobileprogramming.nathanael.pocketguideofcharacters.activities.CreateCharacterActivity;
+import id.ac.ui.ac.mobileprogramming.nathanael.pocketguideofcharacters.database.DatabaseHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        databaseHelper = new DatabaseHelper(MainActivity.this);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.addButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, CreateCharacterActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/id/ac/ui/ac/mobileprogramming/nathanael/pocketguideofcharacters/activities/CreateCharacterActivity.java
@@ -0,0 +1,16 @@
+package id.ac.ui.ac.mobileprogramming.nathanael.pocketguideofcharacters.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import id.ac.ui.ac.mobileprogramming.nathanael.pocketguideofcharacters.R;
+
+public class CreateCharacterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_character);
+    }
+}
